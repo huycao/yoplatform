a53a8b9825bd6a5b82a9f98af7f1f14e254409e8
Phuong Vo <phuong.vm@pinetech.vn>
2016-03-03 17:08:12 +0700

    Interstitial format

---
+++ backend/theme/default/views/partials/codeInterstitialBannerSave.blade.php
@@ -0,0 +1,6 @@
+<script type="text/javascript">
+	var _avlVar = _avlVar || [];
+	_avlVar.push(["{{$wid}}", "{{$zid}}", @if(empty($fwid)) "Interstitial Banner" @else "{{$fwid}}" @endif]);
+	_avlTag = '';
+</script>
+<script "text/javascript" src="{{LINK_AVL}}"></script>  
