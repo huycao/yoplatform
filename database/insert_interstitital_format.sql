1791f180f9c797f31c8c1289cb77518468da373b
Phuong Vo <phuong.vm@pinetech.vn>
2016-03-03 16:56:14 +0700

    Interstitial format

--- database/insert_interstitital_format.sql
+++ database/insert_interstitital_format.sql
@@ -1,1 +1,1 @@
-INSERT INTO `pt_ad_format` (`id`, `type`, `name`, `width`, `height`, `ad_view`, `code_view`, `created_by`, `updated_by`, `created_at`, `updated_at`) VALUES (54,'static','Interstitial Banner',800,500,'interstitial','InterstitialBanner',149,149,'2016-03-03 01:12:02','2016-03-03 01:12:02');
+INSERT INTO `pt_ad_format` (`type`, `name`, `width`, `height`, `ad_view`, `code_view`, `created_by`, `updated_by`, `created_at`, `updated_at`) VALUES ('static','Interstitial Banner',800,500,'interstitial','InterstitialBanner',149,149,'2016-03-03 01:12:02','2016-03-03 01:12:02');
