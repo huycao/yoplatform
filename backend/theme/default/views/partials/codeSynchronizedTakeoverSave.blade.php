693e5001910ba826487ab3a6cf838f9e75961763
Phuong Bui <phuong.bui@pinetech.vn>
2016-03-09 16:47:24 +0700

    add new format synchronized takeover

---
+++ backend/theme/default/views/partials/codeSynchronizedTakeoverSave.blade.php
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+//Place this code where this unit should show in the page only
+var _avlVar = _avlVar || [];
+_avlVar.push(["{{$wid}}", "{{$zid}}",@if(empty($fwid)) "Synchronized Takeover" @else "{{$fwid}}" @endif]);
+_avlTag = '';
+</script>
+<script "text/javascript" src="{{LINK_AVL}}"></script>  
---
+++ database/insert_synchronized_takeover.sql
@@ -0,0 +1,1 @@
+INSERT INTO `pt_ad_format` (`type`, `name`, `width`, `height`, `ad_view`, `code_view`, `created_by`, `updated_by`, `created_at`, `updated_at`) VALUES ('static','Synchronized Takeover',1000,200,'synchronized_takeover','SynchronizedTakeover',149,149,'2016-03-08 01:12:02','2016-03-08 01:12:02');
